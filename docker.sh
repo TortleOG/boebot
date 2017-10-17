a1bf7571c400b027b97c7026c706c816f1c7f1b3
Adam Mahood <mahood.adam@gmail.com>
2017-10-17 15:39:37 -0400

    Add docker.sh

---
+++ docker.sh
@@ -0,0 +1,5 @@
+sudo docker-compose down
+sudo docker-compose pull
+sudo docker-compose up -d
+sudo docker cp public boebot_boebot_1:/usr/src/boebot
+sudo docker cp src/bwd boebot_boebot_1:/usr/src/boebot/src
