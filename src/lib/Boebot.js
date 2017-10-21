6e9487e147f64448a7ba4f957061a409d81dcfcb
Adam Mahood <mahood.adam@gmail.com>
2017-10-20 23:23:16 -0400

    Remove useless docs

--- src/lib/Boebot.js
+++ src/lib/Boebot.js
@@ -9,62 +9,18 @@ const NYTimes = require("./structures/NYTimes");
 const Dashboard = require("./dashboard/Dashboard");
 
 /**
- * @typedef  {object}   OptionsDisabled
- * @property {string[]} [commands=Array]    Disabled Commands
- * @property {string[]} [events=Array]      Disabled Events
- * @property {string[]} [functions=Array]   Disabled Functions
- * @property {string[]} [inhibitors=Array]  Disabled Inhibitors
- * @property {string[]} [finalizers=Array]  Disabled Finalizers
- * @property {string[]} [monitors=Array]    Disabled Monitors
- * @property {string[]} [providers=Array]   Disabled Providers
- * @property {string[]} [extendables=Array] Disabled Extendables
- * @memberof external:KomadaClient
- */
-
-/**
- * @typedef {Object} OptionsProviders
- * @property {string} [engine=json] The Provider Engine SettingGateway will use to store and access to the persistent data.
- * @property {string} [cache=js]    The Provider Cache Engine CacheManager from SettingGateway will use to cache the data.
- * @memberof external:KomadaClient
- */
-
-/**
- * @typedef {Object} KomadaOptions
- * @property {string}  [prefix=?] The prefix for Komada. Defaults to '?'.
- * @property {string}  [ownerID=String] The bot owner's ID, Komada will autofetch it if it's not specified.
- * @property {external:KomadaClient.OptionsDisabled}  [disabled={}] The disabled pieces.
- * @property {PermissionLevels|Array<{}>} [permStructure=Array<{}>] The PermStructure for Komada.
- * @property {boolean} [selfbot=boolean] Whether the bot is a selfbot or not. Komada detects this automatically.
- * @property {string}  [readyMessage=String] A custom string message Komada will use when firing the ready event message.
- * @property {number}  [commandMessageLifetime=1800] The lifetime for the command messages, in milliseconds.
- * @property {number}  [commandMessageSweep=900] How frequent should Komada sweep the command messages.
- * @property {boolean} [disableLogTimestamps=false] Whether the komada logger should show the timestamps.
- * @property {boolean} [disableLogColor=false] Whether the komada logger should show colours.
- * @property {boolean} [cmdEditing=false] Whether Komada should consider edited messages as potential messages able to fire new commands.
- * @property {boolean} [cmdPrompt=false] Whether Komada should prompt missing/invalid arguments at failed command execution.
- * @property {external:KomadaClient.OptionsProviders}  [provider={}] The engines for SettingGateway, 'engine' for Persistent Data, 'cache' for Cache Engine (defaults to Collection)
- * @memberof external:KomadaClient
- */
-
-/**
-  * @typedef {Object} Options
-  * @property {external:KomadaClient.KomadaOptions} options Komadas options
-  * @memberof BoeBot
-  */
-
-/**
-  * The great class for BoeBot
-  * @extends external:KomadaClient
+  * The great class for BoeBot.
+  * @extends external:Komada.KomadaClient
   */
 class BoeBot extends Client {
   /**
-   * Creates a new instance of BoeBot
-   * @param {BoeBot.Options} options The config options to provide Komada
+   * Creates a new instance of BoeBot.
+   * @param {external:Komada.KomadaOptions} options The config options to provide Komada.
    */
   constructor(options) {
     super(options);
     /**
-     * The NYTimes API wrapper
+     * The NYTimes API wrapper.
      * @type {Class}
      */
     this.NYTimes = new NYTimes(settings ? settings.NYT.key : process.env.NYT_API_KEY);
@@ -76,7 +32,7 @@ class BoeBot extends Client {
     this.Currency = null;
 
     /**
-     * The location of all dashboard related files
+     * The location of all dashboard related files.
      * @type {string}
      */
     this.dashboardDir = join(this.clientBaseDir, `../public${sep}`);
@@ -93,8 +49,20 @@ class BoeBot extends Client {
 module.exports = BoeBot;
 
 /**
- * @external KomadaClient
+ * @external Komada
  * @see {@link https://komada.js.org/}
+ */
+
+/**
+ * @typedef {Class} KomadaClient
+ * @memberof external:Komada
+ * @see {@link https://komada.js.org/Komada.html}
+ */
+
+/**
+ * @typedef {Object} KomadaOptions
+ * @memberof external:Komada
+ * @see {@link https://komada.js.org/Komada.html#.Options}
  */
 
 /**
