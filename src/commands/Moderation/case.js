2b185018ad06e4845869d2114d5bbd6e42e463a9
Adam Mahood <mahood.adam@gmail.com>
2017-11-03 20:46:56 -0400

    Add case command

---
+++ src/commands/Moderation/case.js
@@ -0,0 +1,31 @@
+exports.run = async (client, msg, [selected]) => {
+  const { modlogs } = await this.provider.get("modlogs", msg.guild.id);
+  const log = modlogs[selected === "latest" ? modlogs.length - 1 : selected - 1];
+  if (!log) return msg.send(`\`|❌|\` ${msg.author}, there are no modlogs with that case number.`);
+  return msg.send([
+    `Type       : ${log.type}`,
+    `User       : ${log.user.tag} | ${log.user.id}`,
+    `Moderator  : ${log.mod.tag} | ${log.mod.id}`,
+    `Reason     : ${log.reason}`,
+  ], { code: "http" });
+};
+
+exports.init = (client) => { this.provider = client.providers.get("rethinkdb"); };
+
+exports.conf = {
+  enabled: true,
+  runIn: ["text"],
+  aliases: [],
+  permLevel: 2,
+  botPerms: [],
+  requiredFuncs: [],
+  cooldown: 5,
+};
+
+exports.help = {
+  name: "case",
+  description: "Check a case.",
+  usage: "<case:int|latest>",
+  usageDelim: "",
+  extendedHelp: "",
+};
