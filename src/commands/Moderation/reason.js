5c0621c40710466c1810c1f62c1854c55b0fd8b8
Adam Mahood <mahood.adam@gmail.com>
2017-12-12 22:13:03 -0500

    Fix semi error

--- src/commands/Moderation/reason.js
+++ src/commands/Moderation/reason.js
@@ -46,7 +46,7 @@ exports.run = async (client, msg, [selected, ...reason]) => {
   ].join("\n")}${"```"}`);
 };
 
-exports.init = (client) => { this.provider = client.providers.get("rethinkdb") };
+exports.init = (client) => { this.provider = client.providers.get("rethinkdb"); };
 
 exports.conf = {
   enabled: true,
